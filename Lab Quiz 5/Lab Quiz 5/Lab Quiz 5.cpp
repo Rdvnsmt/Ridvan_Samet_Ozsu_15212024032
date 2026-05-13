43e19c1a3085ef273d5318457749270b2f585ef5
ridvansametozsu <sametozsu0@gmail.com>
2026-05-13 12:45:15 +0300

    Lab Quiz 5

---
+++ Lab Quiz 5/Lab Quiz 5/Lab Quiz 5.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+struct Sensor
+{
+    int sensor_id;
+    float sicaklik;
+};
+int main()
+{
+    Sensor Temp;
+    string Student_Id;
+    long long int IdInt;
+    int firstTemp = 0;
+    cout << "Enter your Student id: ";
+    cin >> Student_Id;
+    IdInt = stoll(Student_Id);
+    int N = IdInt % 50;
+    int E = IdInt % 100;
+
+    fstream Sensors("sensors.bin", ios::in | ios::out | ios::binary);
+
+    Sensors.seekg(N * sizeof(Sensor), ios::beg);
+    Sensors.read((char*)&Temp, sizeof(Sensor));
+    firstTemp = Temp.sicaklik;
+    Temp.sicaklik += E;
+
+    Sensors.seekp(-N * sizeof(Sensor), ios::cur);
+    Sensors.write((char*)&Temp, sizeof(Sensor));
+    Sensors.close();
+
+    ofstream Student(Student_Id + "_Quiz5.txt");
+
+    Student << "Number: " + Student_Id << endl;
+    Student << "Sensor Index used in the Process: " << N << endl;
+    Student << "First Temperature: " << firstTemp <<  " -> New Temperature: " << Temp.sicaklik << endl;
+
+    Student.close();
+}
